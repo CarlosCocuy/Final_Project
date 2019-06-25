8c809ca1369cd9531f2cd24dece1c35da1813613
Carlos Cocuy <Carlos.Cocuy@knights.ucf.edu>
2019-06-24 20:14:27 -0400

    split data

---
+++ linear_model.py
@@ -0,0 +1,26 @@
+import math
+import matplotlib
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import time
+
+from datetime import date, datetime, time, timedelta
+from matplotlib import pyplot as plt
+from pylab import rcParams
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from tqdm import tqdm_notebook
+
+%matplotlib inline
+
+#### Input params ##################
+stk_path = "./data/AAPL.csv"
+test_size = 0.2                 # proportion of dataset to be used as test set
+cv_size = 0.2                   # proportion of dataset to be used as cross-validation set
+Nmax = 30                       # for feature at day t, we use lags from t-1, t-2, ..., t-N as features
+                                # Nmax is the maximum N we are going to test
+fontsize = 14
+ticklabelsize = 14
+####################################
